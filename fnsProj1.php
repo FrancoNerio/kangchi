f5c8a3a43e74edb128d8dd2b6066fc0a89a77697
FrancoNerio <franco_sumampong@umindanao.edu.ph>
2017-02-02 10:50:53 +0000

    sample 3

--- fnsProj1.php
+++ fnsProj1.php
@@ -4,5 +4,7 @@
         <h1><p>Hello World</p></h1>
         <br>
         <h3><p>Franco Nerio KangChi Sumampong</h3></p>
+        <br>
+        <button type="button" onclick="alert('salamat sa pag tuplok!')">Click me dayon!</button>
     </body>
 </html>
