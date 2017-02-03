4e8d4c49f926c3c36752bd46569c029e3c8461e4
FrancoNerio <franco_sumampong@umindanao.edu.ph>
2017-02-03 03:34:55 +0000

    updated

--- fnsProj1.php
+++ fnsProj1.php
@@ -3,6 +3,8 @@
     <body>
         <h1><p>Hello World</p></h1>
         <br>
+        <marquee><h2>WeB.Tech</marquee></h2>
+        <br>
         <h3><p>Franco Nerio KangChi Sumampong</h3></p>
         <br>
         <button type="button" onclick="alert('salamat sa pag tuplok!')">Click me dayon!</button>
